15ed6ac26412562d86a24171b511e54f58d6c6b4
Ilia <samoilovilia@mail.ru>
2021-09-04 23:28:07 +0300

    first commit

---
+++ src/main/java/User.java
@@ -0,0 +1,122 @@
+// https://javarush.ru/groups/posts/605-junit
+
+import java.util.*;
+
+public class User {
+    private int id;
+    private String name;
+    private int age;
+    private Sex sex;
+    private static Map<Integer, User> allUsers = new HashMap<>();
+    private static int countId = 0;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public Sex getSex() {
+        return sex;
+    }
+
+    public User(String name, int age, Sex sex) {
+        this.name = name;
+        this.age = age;
+        this.sex = sex;
+
+        if (!hasUser()) {
+            countId++;
+            this.id = countId;
+            allUsers.put(id, this);
+        }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return age == user.age &&
+                Objects.equals(name, user.name) &&
+                Objects.equals(sex, user.sex);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, age, sex);
+    }
+
+    private boolean hasUser() {
+        for (User user : allUsers.values()) {
+            if (user.equals(this) && user.hashCode() == this.hashCode())
+                return true;
+        }
+        return false;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", age=" + age +
+                ", sex=" + sex +
+                '}';
+    }
+
+    public static List<User> getAllUsers() {
+        return new ArrayList<>(allUsers.values());
+    }
+
+    public static List<User> getAllUsers(Sex sex) {
+        List<User> listAllUsers = new ArrayList<>();
+        for (User user : allUsers.values()) {
+            if (user.sex == sex)
+                listAllUsers.add(user);
+        }
+        return listAllUsers;
+    }
+
+    public static int getHowManyUsers(){
+        return allUsers.size();
+    }
+
+    public static int getHowManyUsers(Sex sex){
+        return getAllUsers(sex).size();
+    }
+
+    public static int getAllAgeUsers(){
+        int countAge = 0;
+        for (User user : allUsers.values()){
+            countAge += user.age;
+        }
+        return countAge;
+    }
+
+    public static int getAllAgeUsers(Sex sex){
+        int countAge = 0;
+        for (User user : getAllUsers(sex)){
+            countAge += user.age;
+        }
+        return countAge;
+    }
+
+    public static int getAverageAgeOfAllUsers(){
+        return getAllAgeUsers() / getHowManyUsers();
+    }
+
+    public static int getAverageAgeOfAllUsers(Sex sex){
+        return getAllAgeUsers(sex) / getHowManyUsers(sex);
+    }
+
+
+
+
+}
